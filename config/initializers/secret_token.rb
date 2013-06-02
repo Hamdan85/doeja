5f8ec5bd7ff3a220a26b9411ab3e23e20c196d2a
Gabriel Hamdan <ghamdan.eng@gmail.com>
2013-06-01 23:53:23 -0300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doeja::Application.config.secret_token = '95844cb0c7d243f177e85659f200b0a3a0b4363d846c0c5dde996bc7ce51fc31e3a894e31b0311cf398584106bf0b9f0f64928a2b2c04957abe1b97486bb7867'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Doeja::Application.config.session_store :cookie_store, key: '_doeja_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Doeja::Application.config.session_store :active_record_store
