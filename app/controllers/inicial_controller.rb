b98e080bcbe20f22934e4317ef0ed5dc19b856b8
Gabriel Hamdan <ghamdan.eng@gmail.com>
2013-08-13 00:58:51 -0300

    Working Again With Geocoder
    Misteryously got back to work withou any change.
    Kinda afraid.
    
    Lost hours programming for nothing.
    At least i found a way to ask json directly from server. Now, if i want to, i can make my own geocoded. YAY!

--- app/controllers/inicial_controller.rb
+++ app/controllers/inicial_controller.rb
@@ -12,26 +12,26 @@ class InicialController < ApplicationController
 
   def create
 
-      @Receiver = Receiver.new(params[:receiver])
-      @Receiver.token = loop do
-        random_token = SecureRandom.urlsafe_base64
-        break random_token unless Receiver.where(token: random_token).exists?
-      end
+    @Receiver = Receiver.new(params[:receiver])
+    @Receiver.token = loop do
+      random_token = SecureRandom.urlsafe_base64
+      break random_token unless Receiver.where(token: random_token).exists?
+    end
 
-      respond_to do |format|
-        if @Receiver.save
-          UserRegistration.userRegister(@Receiver).deliver
-          @alert = 'Local salvo com sucesso. Muito Obrigado!!'
-          format.html
-          format.js
-        else
-          @Receiver.errors.to_a.each do |item|
-            @errors = "#{@errors} \n + #{item}".to_s
-          end
-          format.js { render 'inicial/createerror' }
+    respond_to do |format|
+      if @Receiver.save
+        UserRegistration.userRegister(@Receiver).deliver
+        @alert = 'Local salvo com sucesso. Muito Obrigado!!'
+        format.html
+        format.js
+      else
+        @Receiver.errors.to_a.each do |item|
+          @errors = "#{@errors} \n + #{item}".to_s
         end
-
+        format.js { render 'inicial/createerror' }
       end
+
+    end
 
   end
 
@@ -39,33 +39,25 @@ class InicialController < ApplicationController
 
     #Geocoding Users Address
 
-    require 'open-uri'
-    require 'json'
+    @useraddress = "#{params[:Donate][:address]}"
 
-    @useraddress = "http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:Donate][:address]}&sensor=true".gsub(' ', '+').to_s
+    userlocation = Geocoder.search(@useraddress)[0]
 
-    json_object = open(@useraddress).read
-
-    userlocation = JSON.parse(json_object)["results"].first
-
-    #find city on Google's Json Response
-
-    city = ""
-
-    userlocation["address_components"].each do |ul|
-      if ul["types"] == [ "locality", "political" ]
-        city = ul["long_name"]
-      end
-    end
 
     #Assembling the JSON Hash of user's Marker
 
-    usermarkhash = {:title => 'Você', :description => 'Você está aqui!', :animation => 'BOUNCE', :picture => '/assets/user.png', :height => 64, :width => 64, :lat => userlocation["geometry"]["location"]["lat"], :lng => userlocation["geometry"]["location"]["lng"] }
+    usermarkhash = {:title => 'Você', :description => 'Você está aqui!', :animation => 'BOUNCE', :picture => '/assets/user.png', :height => 64, :width => 64, :lat => userlocation.latitude, :lng => userlocation.longitude}
 
     #Looking for near place to receive the donation
 
-    @receiving = Receiver.where("lower(receiving) =? and lower(city) = ?", params[:Donate][:receiving].downcase,city.downcase).to_gmaps4rails do |address, marker|
-      distance = Geocoder::Calculations.distance_between([userlocation["geometry"]["location"]["lat"],userlocation["geometry"]["location"]["lng"]],[address["latitude"],address["longitude"]])/1.609344
+    @receiving = Receiver.where("lower(receiving) =? and lower(city) = ?", params[:Donate][:receiving].downcase,userlocation.city.downcase).to_gmaps4rails do |address, marker|
+      #dlat = userlocation.latitude-address["latitude"]
+      #dlon = userlocation.longitude-address["longitude"]
+      #a =  (Math.sin(dlat/2))**2 + Math.cos(userlocation.latitude) * Math.cos(address["latitude"]) * (Math.sin(dlon/2))**2
+      #c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a) )
+      #distance = (3961 * c)/1.609344
+
+      distance = Geocoder::Calculations.distance_between([userlocation.latitude,userlocation.longitude],[address["latitude"],address["longitude"]])/1.609344
 
       marker.infowindow render_to_string(:partial => '/shared/mapbox', :locals => { :address => address, :distance => distance })
       marker.picture({
@@ -74,6 +66,8 @@ class InicialController < ApplicationController
                          :height  => 32
                      })
       marker.json({ :distance => distance })
+
+
     end
 
     if @receiving == '[]'
@@ -83,6 +77,8 @@ class InicialController < ApplicationController
     hash = JSON.parse(@receiving)
     hash.each do |item|
 
+
+      #item['distance'] = Geocoder::Calculations.distance_between([userlocation.latitude,userlocation.longitude],[address["latitude"],item["lng"]])/1.609344
     end
 
     @receiving = hash
@@ -99,12 +95,25 @@ class InicialController < ApplicationController
     end
   end
 
+  def addressautocomplete
+
+    require 'open-uri'
+
+    address = params[:term]
+    addresscomplete =  JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?address=#{address.gsub(' ', '+')}&sensor=true").read)['results']
+
+    respond_to do |format|
+      format.json { render json: addresscomplete }
+      format.js
+    end
+
+
+  end
+
 
   def donationkind
 
     parameters = params[:term].downcase
-
-
     if parameters.nil? or parameters == 'receber...' or parameters == ''
       @tipodoacao = Receiver.all
     else
@@ -114,8 +123,8 @@ class InicialController < ApplicationController
     x = 0
     json = []
     @tipodoacao.uniq.pluck(:receiving).each do |item|
-        json.push({"id" => x, "label" => item })
-        x = x+1
+      json.push({"id" => x, "label" => item })
+      x = x+1
     end
 
     if @tipodoacao
